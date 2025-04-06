6898626b2dfbeab6f558871c51d96aeba31459bd
khannaveed2310 <khan23naveed@gmail.com>
2025-04-06 16:23:46 +0530

    Generate PassWord

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
 const PasswordBox = document.getElementById("password");
-const Generate = document.getElementById("Generate")
-const Copy = document.getElementById("Copy")
-const length = 10;
+const Generate = document.getElementById("Generate");
+const Copy = document.getElementById("Copy");
+const LengthRange = document.getElementById("lengthRange"); // <- New line
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase ="abcdefghijklmnopqrstuvwxyz";
-const number ="0123456789"
-const special ="~!@#$%^&*()-_+={}[]|/;<>,.?";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const special = "~!@#$%^&*()-_+={}[]|/;<>,.?";
+const allChars = upperCase + lowerCase + number + special;
 
-const allChars = upperCase + lowerCase + number + special
 
 function createPassword() {
+    const length = parseInt(LengthRange.value); // <- Use selected length
     let password = "";
 
-    password += upperCase[Math.floor(Math.random() * upperCase.length)]
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
-    password += number[Math.floor(Math.random() * number.length)]
-    password += special[Math.floor(Math.random() * special.length)]
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += special[Math.floor(Math.random() * special.length)];
 
-    // console.log(password)
-
-    while(length > password.length){
-        password += allChars[Math.floor(Math.random() * allChars.length)]
+    while (password.length < length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
     }
 
-    PasswordBox.value = password
+    PasswordBox.value = password;
 }
 
-Generate.addEventListener("click", function(){
+Generate.addEventListener("click", function () {
     createPassword()
 })
 
 
-function copyPassword(){
+function copyPassword() {
     PasswordBox.select()
     document.execCommand("copy")
 }
 
-Copy.addEventListener("click" , function(){
+Copy.addEventListener("click", function () {
     copyPassword()
 })
-
-// createPassword()
